d7961597fa83d15a802582f6af35cf23d56890b3
cdenneen <cdenneen@gmail.com>
2014-02-10 22:56:21 -0500

    Update evernote_spec.rb

--- spec/classes/evernote_spec.rb
+++ spec/classes/evernote_spec.rb
@@ -4,7 +4,7 @@ describe 'evernote' do
   it do
     should contain_package('Evernote').with({
       :provider => 'appdmg_eula',
-      :source   => 'http://cdn1.evernote.com/mac/release/Evernote_400995.dmg',
+      :source   => 'http://cdn1.evernote.com/mac/release/Evernote_402491.dmg',
     })
   end
 end
